820fc1f9939e6d4cca5876e6697bbc7ad4e63200
Antoine Chapon <chapon.antoine@gmail.com>
2022-11-30 16:16:21 -0500

    -

--- main.R
+++ main.R
@@ -168,19 +168,21 @@ levd(x = pot_surge$y,
 
 init_surge$results$par
 
+
+
+
 bdmcmc_nhpp <- function(input,
                         thresh,
                         fevd_init,
                         n_mc = 1e4) {
   # BDMCMC with mu and sigma dependent on linear or quadratic time and season.
   # Using phi instead of sigma.
-  
+  library(mvtnorm)
   t_vec <- scale(1:nrow(input))
-
   # Lists of vectors indication which covariate is currently active.
   locat_l <- list(
     trend = rep(FALSE, 2),
-    season = F
+    season = FALSE
   )
   scale_l <- list(
     trend = rep(FALSE, 2),
@@ -190,68 +192,203 @@ bdmcmc_nhpp <- function(input,
   # Matrices of covariates.
   trend <- cbind(t_vec, t_vec^2)
   season <- cbind(input$cos, input$sin)
-
   # Initializing the parameter vector and the loglikelihood.
   par_old <- list(
-    mu = fevd_init$results$par["location"],
-    phi = fevd_init$results$par["log.scale"],
+    mu = c(fevd_init$results$par["location"], rep(NA, 4)),
+    phi = c(fevd_init$results$par["log.scale"], rep(NA, 4)),
     xi = fevd_init$results$par["shape"]
   )
   lik_old <- exp(-fevd_init$results$value)
-  
   n_par <- unlist(fevd_init$results$num.pars)
-  
+  # Function to optimize to get mean(s) of the jump proposals.
+  optim_fevd <- function(x, par_l, n_hyp) {
+    cov_mu <- cbind(
+      rep(1, length(nrow(input))),
+      trend[, par_l[[1]]$trend],
+      season[, rep(par_l[[1]]$season, 2)]
+    )
+    cov_phi <- cbind(
+      rep(1, length(nrow(input))),
+      trend[, par_l[[2]]$trend],
+      season[, rep(par_l[[2]]$season, 2)]
+    )
+    mu_vec <- cov_mu %*% x[which(n_hyper == 1)]
+    phi_vec <- cov_phi %*% x[which(n_hyper == 2)]
+    xi_vec <- x[which(n_hyper == 3)]
+    levd(x = input$y,
+         threshold = thresh,
+         location = mu_vec,
+         scale = exp(phi_vec),
+         shape = xi_vec,
+         type = "PP")
+  }
   out <- list()
-  
   for (i in 1:n_mc) {
-    # Stay step: update parameters with Metropolis-Hastings (actually only
-    # Metropolis for mu and phi).
-    
+    # Stay step: update every parameter with Metropolis. (NEED BOUNDED MH FOR XI ?)
     for (j in 1:length(par_old)) {
       # Iterating EVD parameters.
-      for (k in length(par_old[[j]])) {
+      for (k in 1:length(par_old[[j]])) {
         # Iterating the jth parameter hyperparameters.
-        par_new <- par_old
-        
-        par_new[[j]][k] <- rnorm(1, par_old[[j]][k], 1e-1)
-        
-        # Assemble the covariate matrix.
-        cov_mu <- cbind(
-          rep(1, length(nrow(input))),
-          trend[, par_l[[1]]$trend],
-          season[, par_l[[1]]$season]
-        )
-        cov_phi <- cbind(
-          rep(1, length(nrow(input))),
-          trend[, par_l[[2]]$trend],
-          season[, par_l[[2]]$season]
-        )
-        # Compute de vector of the parameters.
-        mu_vec <- cov_mu %*% par_new[[1]]
-        phi_vec <- cov_phi %*% par_new[[2]]
-        xi_vec <- rep(par_new[[3]], nrow(input)) # Just in case varying xi is implemented later.
-        
-        # Likelihood of new model.
-        lik_new <- levd(x = input$y,
-                        threshold = thresh,
-                        location = mu_vec,
-                        scale = exp(phi_vec),
-                        shape = xi_vec,
-                        type = "PP",
-                        log = FALSE,
-                        negative = FALSE)
-        
-        alpha <- lik_new / lik_old
-        
-        if (runif(1) < alpha) {
-          par_old[[j]][k] <- par_new[[j]][k]
-          lik_old <- lik_new
+        if (!is.na(par_old[[j]][k])) {
+          par_new <- par_old
+          par_new[[j]][k] <- rnorm(1, par_old[[j]][k], 1e-1)
+          # Assemble the covariate matrix.
+          cov_mu <- cbind(
+            rep(1, length(nrow(input))),
+            trend[, par_l[[1]]$trend],
+            season[, rep(par_l[[1]]$season, 2)] # Need to take both columns.
+          )
+          cov_phi <- cbind(
+            rep(1, length(nrow(input))),
+            trend[, par_l[[2]]$trend],
+            season[, rep(par_l[[2]]$season, 2)]
+          )
+          # Compute de vector of the parameters.
+          mu_vec <- cov_mu %*% na.omit(par_new[[1]])
+          phi_vec <- cov_phi %*% na.omit(par_new[[2]])
+          xi_vec <- rep(par_new[[3]], nrow(input)) # Just in case varying xi is implemented later.
+          # Likelihood of the new model.
+          lik_new <- levd(x = input$y,
+                          threshold = thresh,
+                          location = mu_vec,
+                          scale = exp(phi_vec),
+                          shape = xi_vec,
+                          type = "PP",
+                          log = FALSE,
+                          negative = FALSE)
+          # Acceptance probability for stay step.
+          alpha <- lik_new / lik_old
+          if (runif(1) < alpha) {
+            par_old[[j]][k] <- par_new[[j]][k]
+            lik_old <- lik_new
+          }
+          out <- list.append(out, unlist(par_old)) # ITS BAD TO GROW LIST ON MCMC ?
+          model_id <- paste(as.numeric(unlist(par_l)), collapse = "")
+          names(out)[length(out)] <- model_id
         }
-        out <- list.append(out, unlist(par_old)) # ITS BAD TO GROW LIST ON MCMC ?
-        model_id <- paste(as.numeric(unlist(par_l)), collapse = "")
-        names(out)[length(out)] <- model_id
       }
     }
+    # Jump step:
+    # Pick a parameter for the jump.
+    idx_par <- floor(runif(1, 1, length(par_l) + 1))
+    # Pick a covariate of this parameter.
+    idx_cov <- floor(runif(1, 1, length(par_l[[idx_par]]) + 1))
+    # "idx_cov" also corresponds to the number of hyperparameter to change for
+    # each covariate when 1=trend and 2= season, so used as such later (but bad
+    # for generalisation of function to other covariates).
+    # Select move (switching last T to F or first F to T).
+    len_cov <- length(par_l[[idx_par]][[idx_cov]])
+    sum_cov <- sum(par_l[[idx_par]][[idx_cov]])
+    if (sum_cov == 0) {
+      sum_new <- 1
+    } else if (sum_cov == len_cov) {
+      sum_new <- len_cov - 1
+    } else {
+      sum_new <- sum_cov + sign(rnorm(1))
+    }
+    # Compute jump and reverse jump relative probabilities.
+    if (sum_cov == 0 | sum_cov == len_cov) {
+      p_j <- 1
+    } else {
+      p_j <- .5
+    }
+    if (sum_new == 0 | sum_new == len_cov) {
+      p_rj <- 1
+    } else {
+      p_rj <- .5
+    }
+    vec_new <- c(rep(TRUE, sum_new), rep(FALSE, len_cov - sum_new))
+    par_l_new <- par_l
+    par_l_new[[idx_par]][[idx_cov]] <- vec_new
+    diff_dir <- sum(par_l_new[[idx_par]][[idx_cov]]) - sum_cov
+    if (diff_dir > 0) {
+      # Adding hyperparameter(s). Need to optimize their values to get mean
+      # of the proposal (as in Min & Czado 2011 for the vine copula).
+      par_new <- par_old
+      mu_bool <- c(par_l_new[[1]][[1]], rep(par_l_new[[1]][[2]], 2))
+      phi_bool <- c(par_l_new[[2]][[1]], rep(par_l_new[[2]][[2]], 2))
+      par_new[[1]][c(FALSE, mu_bool)] <- Inf
+      par_new[[2]][c(FALSE, phi_bool)] <- Inf
+      vec <- na.omit(unlist(par_new))
+      x0 <- vec
+      lb <- vec
+      ub <- vec
+      n_hyper <- c(
+        rep(1, length(na.omit(par_new[[1]]))),
+        rep(2, length(na.omit(par_new[[2]]))),
+        3
+      )
+      x0[which(x0 == Inf)] <- 0
+      lb[which(lb == Inf)] <- -10
+      ub[which(ub == Inf)] <- 10
+      opt <- nloptr(
+        x0 = x0,
+        eval_f = optim_fevd,
+        lb = lb,
+        ub = ub,
+        opts = list(
+          algorithm = "NLOPT_LN_SBPLX", # bug with "NLOPT_LN_COBYLA"
+          ftol_rel = 1e-2,
+          max_eval = -1,
+          print_level = 0
+        ),
+        par_l = par_l_new,
+        n_hyp = n_hyper
+      )
+      # Removing the first hyperparameter that is never concerned by jumps.
+      prop_mean <- opt$solution[n_hyper == idx_par][-1]
+      draw <- rmvnorm(1,
+                      mean = prop_mean,
+                      sigma = diag(1e-2,
+                                   length(prop_mean),
+                                   length(prop_mean)))
+      par_new[[idx_par]][which(par_new[[idx_par]] == Inf)] <- draw
+    } else {
+      
+    }
+    # Assemble the covariate matrix.
+    cov_mu <- cbind(
+      rep(1, length(nrow(input))),
+      trend[, par_l_new[[1]]$trend],
+      season[, rep(par_l_new[[1]]$season, 2)]
+    )
+    cov_phi <- cbind(
+      rep(1, length(nrow(input))),
+      trend[, par_l_new[[2]]$trend],
+      season[, rep(par_l_new[[2]]$season, 2)]
+    )
+    # Compute de vector of the parameters.
+    mu_vec <- cov_mu %*% na.omit(par_new[[1]])
+    phi_vec <- cov_phi %*% na.omit(par_new[[2]])
+    xi_vec <- rep(par_new[[3]], nrow(input)) # Just in case varying xi is implemented later.
+    # Likelihood of the new model.
+    lik_new <- levd(x = input$y,
+                    threshold = thresh,
+                    location = mu_vec,
+                    scale = exp(phi_vec),
+                    shape = xi_vec,
+                    type = "PP",
+                    log = FALSE,
+                    negative = FALSE)
+    
+    
+    
+    # dens_old <- dmvnorm(1,
+    #                     mean = idx_cov,
+    #                     sigma = diag(1e-2,
+    #                                  length(idx_cov),
+    #                                  length(idx_cov)))
+    
+    # Acceptance probability for jump step.
+    # Priors cancel so not included.
+    alpha <- lik_new / lik_old * p_rj / p_j * 
+    if (runif(1) < alpha) {
+      par_old[[j]][k] <- par_new[[j]][k]
+      lik_old <- lik_new
+    }
+    out <- list.append(out, unlist(par_old))
+    model_id <- paste(as.numeric(unlist(par_l)), collapse = "")
+    names(out)[length(out)] <- model_id
   }
   return(out)
 }
@@ -260,9 +397,12 @@ zut <- bdmcmc_nhpp(pot_surge,
                    thresh = thresh_surge$fitted.values,
                    fevd_init = init_surge,
                    n_mc = 1e2)
+zut
 
 zut2 <- bind_rows(zut)
 
-hist(zut2$mu0, 30)
-hist(zut2$mu1, 30)
-hist(zut2$log.scale, 30)
+hist(zut2$mu.location, 30)
+hist(zut2$phi.log.scale, 30)
+hist(zut2$xi.shape, 30)
+
+
